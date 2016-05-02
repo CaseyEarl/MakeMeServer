84092145f695db4772e5dca87854a7d897389517
Casey Earl <caseyearl@D-10-156-15-31.dhcp4.washington.edu>
2016-05-02 14:03:21 -0700

    Add reminder and List models

---
+++ models/list.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+mongoose.set('debug', true);
+
+var Schema = mongoose.Schema;
+
+var listSchema = new Schema(
+	{
+		title: String,
+		remindersList:[{type: Schema.Types.ObjectId, ref: 'Reminder'}],
+		to: {type: Schema.Types.ObjectId, ref: 'User'},
+		from: {type: Schema.Types.ObjectId, ref: 'User'},
+	},
+	{
+        timestamps: {createdAt: 'creation_date', completed: 'last_modified'}
+    }
+);
+
+var List = module.exports = mongoose.model('List', listSchema);
+
+module.exports.saveList = function(newList, callback){
+	newList.save(callback);
+};
---
+++ models/reminder.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+mongoose.set('debug', true);
+
+var Schema = mongoose.Schema;
+
+var reminderSchema = new Schema(
+	{
+		task: String,
+		alarm: Date,
+		status: String,
+	},
+	{
+        timestamps: {createdAt: 'creation_date', completed: 'last_modified'}
+    }
+);
+
+var Reminder = module.exports = mongoose.model('Reminder', reminderSchema);
+
+module.exports.saveReminder = function(newReminder, callback){
+	newReminder.save(callback);
+};
